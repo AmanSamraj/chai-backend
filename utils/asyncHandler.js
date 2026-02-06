066a0c350097822373b59702f5c0516657cb8dc9
AmanSamraj <amansamraj@gmail.com>
2026-02-06 05:42:39 +0530

    all set configured

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -12,13 +12,21 @@
 // const asyncHandler =(func) => () => {}
 // const asyncHandler =(func) => async() => {}
 
+// const asyncHandler =(fn) => async(req,res,next) => {
+//     try{
+//         await fn(req, res, next )
+//     }catch (error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message:err.message
+//         })
+//     }
+// }
 const asyncHandler =(fn) => async(req,res,next) => {
-    try{
-        await fn(req, res, next )
-    }catch (error){
-        res.status(error.code || 500).json({
-            success: false,
-            message:err.message
-        })
+    return (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch
+        ((err)=> next(err))
     }
 }
+
+export{asyncHandler}
